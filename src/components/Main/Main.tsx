e48a1bd5bb0f6125bc3b2f4283dee561479aa0aa
faresdevweb <fares.meier@hotmail.com>
2023-11-19 15:12:41 +0100

    feat: added main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import "./Main.scss";
+import HeaderDate from "../HeaderDate/HeaderDate";
+
+const Main = () => {
+  return (
+    <div className="main_container">
+      <HeaderDate />
+    </div>
+  );
+};
+
+export default Main;
